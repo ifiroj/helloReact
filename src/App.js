907477c0c4e090ef327c7c150915e18e9eb66806
ifiroj <firoj.acharya@gmail.com>
2022-11-05 19:24:03 +0545

    -

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return <div>
-        <h1>Hello react.</h1>
+      <h1>Hello React.</h1>
     </div>
   }
 }
